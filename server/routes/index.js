627ea8a86b19f576b6687636b72a8561b5c1be23
osiz <deepak@osiztech.com>
2019-05-15 13:02:49 +0530

    login app

--- server/routes/index.js
+++ server/routes/index.js
@@ -29,7 +29,7 @@ router.post('/login', async function(req, res, next) {
 		const params = req.body;
 		var exists = await userTable.findOne(params);
 		if(!exists) throw 'not_found';
-		res.send({status:true,message:'Successfully registered',result:already_exists});
+		res.send({status:true,message:'Valid credentials. Logging you in...',result:exists._id});
 	}
 	catch(e){
 		switch(e){
@@ -37,6 +37,27 @@ router.post('/login', async function(req, res, next) {
 				res.send({status:false,message:'Username or password is invalid'});
 			break;
 			default:
+			console.log(e);
+				res.send({status:false,message:'Something went wrong'});
+			break;
+		}
+	}
+});
+
+router.post('/get_user/:id', async function(req, res, next) {
+	try{
+		const id = req.params.id;
+		var exists = await userTable.findOne({_id:id});
+		if(!exists) throw 'not_found';
+		res.send({status:true,message:'',result:exists});
+	}
+	catch(e){
+		switch(e){
+			case 'not_found':
+				res.send({status:false,message:'No records found'});
+			break;
+			default:
+			console.log(e);
 				res.send({status:false,message:'Something went wrong'});
 			break;
 		}
