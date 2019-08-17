f3ac9906e0c1df5ebdc2ec2b7912e8dc55e8cb8d
Deepak Raj M <deepak@osiztech.com>
2019-08-17 20:02:32 +0530

    myapp

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,6 +4,8 @@ module.exports = {
     script: 'bin/www',
     instances: 1,
     autorestart: true,
+    restart_delay : 5000,
+    ignore_watch : ['logs'],
     watch: true,
     max_memory_restart: '1G',
     env: {
